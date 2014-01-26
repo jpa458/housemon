c61f8d50d3d27666455955f12761c45a84c2b919
Jean-Claude Wippler <jc@wippler.nl>
2014-01-26 17:19:12 +0100

    star on RF12demo decoder, logger code has been moved to JB

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	"log"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/jcw/jeebus"
 )
 
-const LOGGER_PATH_FMT = "./logger/%d"
-
-var currentLogFile *os.File
+var (
+	rdClient jeebus.Client
+)
 
 func main() {
 	if len(os.Args) <= 1 {
@@ -21,43 +20,31 @@ func main() {
 
 	switch os.Args[1] {
 
-	case "logger":
-		logger()
+	case "decode":
+		decode()
 
 	default:
 		log.Fatal("unknown sub-command: housemon ", os.Args[1], " ...")
 	}
 }
 
-func logger() {
-	for msg := range jeebus.ConnectToServer("rd/#") {
-		text := msg.Get("text")
-		now := time.Now().UTC()
-		datePath := dateFilename(now)
-		if currentLogFile == nil || datePath != currentLogFile.Name() {
-			if currentLogFile != nil {
-				currentLogFile.Close()
-			}
-			mode := os.O_WRONLY | os.O_APPEND | os.O_CREATE
-			fd, err := os.OpenFile(datePath, mode, os.ModePerm)
-			if err != nil {
-				log.Fatal(err)
-			}
-			currentLogFile = fd
-		}
-		// L 01:02:03.537 usb-A40117UK OK 9 25 54 66 235 61 210 226 33 19
-		h, m, s := now.Clock()
-		port := strings.SplitN(msg.T, "/", 3)[2]
-		line := fmt.Sprintf("L %02d:%02d:%02d.%03d %s %s\n",
-			h, m, s, now.Nanosecond()/1000000, port, text)
-		currentLogFile.WriteString(line)
-	}
+func decode() {
+	rdClient.Connect("rd")
+	rdClient.Register("RF12demo/#", &RF12demoDecodeService{})
+
+	<-make(chan byte) // wait forever
 }
 
-func dateFilename(now time.Time) string {
-	year, month, day := now.Date()
-	path := fmt.Sprintf(LOGGER_PATH_FMT, year)
-	os.MkdirAll(path, os.ModePerm)
-	// e.g. "./logger/2014/20140122.txt"
-	return fmt.Sprintf("%s/%d.txt", path, (year*100+int(month))*100+day)
+type RF12demoDecodeService struct {
+}
+
+func (s *RF12demoDecodeService) Handle(m *jeebus.Message) {
+	// log.Println("RF12", m)
+	text := m.Get("text")
+	if strings.HasPrefix(text, "[RF12demo.") {
+		fmt.Println(text)
+	}
+	if strings.HasPrefix(text, "OK ") {
+		fmt.Println(text)
+	}
 }
