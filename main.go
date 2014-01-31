590e20d0a07b0dc151346d847def3f95e4ccf5f5
SevenW <info@sevenwatt.com>
2014-01-31 17:26:58 +0100

    introduce rf12demo node mapping and forwarding of rf12/<nodetype> topics

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 	"strings"
 
 	"github.com/jcw/jeebus"
+	"github.com/jcw/housemon/drivers"
+	"regexp"
 )
+
+var confRegex = regexp.MustCompile(`^ [A-Z[\\\]\^_@] i(\d+)(\*)? g(\d+) @ (\d\d\d) MHz`)
 
 func main() {
 	if len(os.Args) <= 1 {
@@ -21,6 +25,15 @@ func main() {
 	case "decode":
 		rdClient := jeebus.NewClient("rd")
 		rdClient.Register("RF12demo/#", &RF12demoDecodeService{})
+
+		drivers.JNodeMap()
+		msg := map[string]interface{}{"text": "v"}
+		jeebus.Publish("if/RF12demo", msg)
+		msg = map[string]interface{}{"text": "c"}
+		jeebus.Publish("if/RF12demo", msg)
+		//"text":" A i1 g178 @ 868 MHz "
+
+
 		<-make(chan byte) // wait forever
 
 	default:
@@ -31,12 +44,29 @@ func main() {
 type RF12demoDecodeService struct {
 }
 
+var node, grp, band int
+var astx bool
+var (
+	rf12msg struct {
+		ID   [2]int `json:"id"`
+		Dev  string `json:"dev"`
+		Loc string `json:"loc"`
+		Text string `json:"text"`
+		Time int64  `json:"time"`
+	}
+)
+
 func (s *RF12demoDecodeService) Handle(m *jeebus.Message) {
 	text := m.Get("text")
+
 	if strings.HasPrefix(text, "OK ") {
 		var buf bytes.Buffer
+		var vals []string
+		vals = strings.Split(text[3:], " ")
+		rnode , err := strconv.Atoi(vals[0])
+		check(err)
 		// convert the line of decimal byte values to a byte buffer
-		for _, v := range strings.Split(text[3:], " ") {
+		for _, v := range vals {
 			n, err := strconv.Atoi(v)
 			check(err)
 			buf.WriteByte(byte(n))
@@ -44,6 +74,23 @@ func (s *RF12demoDecodeService) Handle(m *jeebus.Message) {
 		now := m.GetInt64("time")
 		dev := strings.SplitN(m.T, "/", 2)[1]
 		fmt.Printf("%d %s %X\n", now, dev, buf.Bytes())
+		rf12msg.ID[0] = 178
+		rf12msg.ID[1] = 22
+		rf12msg.Dev = dev
+		rf12msg.Text = text
+		rf12msg.Time = now
+		if found, nT, nL := drivers.JNodeType(grp, rnode); found {
+			rf12msg.Loc = nL
+			jeebus.Publish("rf12/"+nT, rf12msg)
+		} else {
+			jeebus.Publish("rf12/unknown", rf12msg)
+		}
+	} else if conf := confRegex.FindStringSubmatch(text); conf != nil {
+		node, _ = strconv.Atoi(conf[1])
+		astx = conf[2] == "*"
+		grp, _ = strconv.Atoi(conf[3])
+		band, _ = strconv.Atoi(conf[4])
+		fmt.Println("groupID: ", grp)
 	}
 }
 
